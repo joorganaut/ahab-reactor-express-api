df32163c8fad569f136bce1c3b79f5042f69b77d
Joorgi Spectra <sazeespectra@gmail.com>
2020-06-12 21:02:33 +0100

    clear

--- bin/services/USSDProcessorServer.js
+++ bin/services/USSDProcessorServer.js
@@ -11,10 +11,11 @@ class USSDProcessorServer {
         this.next = next;
     }
     async ProcessRequest() {
+        let request = new USSDRequest(this.req.body);
+        console.log(JSON.stringify(request));
         let response = new USSDResponse({Error : ''});
         try {
-            let request = new USSDRequest((this.req.body));
-            console.log(JSON.stringify(request));
+            
             if (request.content === null || request.content === undefined || request.content === '' || request.content === "*372*2#") {
                 response.content = "Invalid PIN. Please dial *372*2*PIN# to recharge";
                 response.msisdn = request.msisdn;
